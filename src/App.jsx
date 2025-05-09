e5050d8867475cc6cc53bcedaf7348f3fc9e939a
Harry Holmes <harryholmesse@gmail.com>
2025-05-09 12:29:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes } from 'react-router-dom'
+import './assets/CSS/main.css'
+import CenterLayout from './assets/layouts/CenterLayout.jsx'
+import PortalLayout from './assets/layouts/PortalLayout.jsx'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Routes>
+      </Routes>
+    </>
+  )
+}
+
+export default App
