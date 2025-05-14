ad1b0c9a3ab7d15ccf76c3875089d582cb8f0fa2
Harry Holmes <harryholmesse@gmail.com>
2025-05-14 11:05:28 +0200

    styling of footer for mobile mode

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -4,7 +4,21 @@ const Footer = () => {
   return (
     <footer>
       <div class="footer-container">
-        Footer      
+        <div class="copyright">
+          <h6>Copyright © 2025 Peterdraw</h6>
+        </div>
+        <div class="footer-links">
+          <p>Privacy Policy</p>
+          <p>Terms and conditions</p>
+          <p>Contact Us</p>
+        </div>
+        <div class="socials">
+          <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-x-twitter"></i>
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-youtube"></i>
+          <i class="fa-brands fa-linkedin"></i>
+        </div>
       </div>
     </footer>
   )
