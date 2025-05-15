687c97878169f1aac862d61a401b7ad759bc6ece
Harry Holmes <harryholmesse@gmail.com>
2025-05-15 16:50:19 +0200

    Styling of header and nav

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -4,7 +4,41 @@ const Header = () => {
   return (
     <header>
       <div class="header-container">
-        Header      
+        <div class='logo'>
+          <img src='/images/logo.svg' alt="Logo" class="logo"/>
+        </div>
+        <div class="breadcrumb-container">
+          <div class="breadcrumb">
+            <p>Breadcrumb</p>
+          </div>
+          <h2>Page title</h2>
+        </div>
+
+        <div class="more-container">
+          <i class="fa-regular fa-bars"></i>
+
+          <div class="options">
+            <div class="search option">
+              <div class="search-bar">
+                <p>Search anything</p>
+              </div>
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+            <div class="notifications option">
+              <i class="fa-solid fa-bell"></i>
+            </div>
+            <div class="settings option">
+              <i class="fa-solid fa-gear"></i>
+            </div>
+            <div class="profile option">
+              <i class="fa-solid fa-user"></i>
+              <div class="profile-info">
+                <p class="name">User Name</p>
+                <p class="status">User Status</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   )
--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -4,8 +4,22 @@ const Nav = () => {
   return (
     <nav>
       <div class="nav-container">
-        <img src='/images/logo.svg' alt="Logo" class="logo"/>
+        <div class="logo-container">
+          <img src='/images/logo.svg' alt="Logo" class="logo"/>
+          <h2>Ventixe</h2>
+        </div>
          
+
+         <div class="signout-container">
+          <div class="signout-tablet">
+            <i class="fa-regular fa-arrow-right-from-bracket"></i>
+          </div>
+          
+          <div class="signout-desktop">
+            <i class="fa-regular fa-arrow-right-from-bracket"></i>
+            <p>Sign out</p>
+          </div>
+         </div>
       </div>
     </nav>
   )
